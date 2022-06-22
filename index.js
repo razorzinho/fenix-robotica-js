3360dc4558a7b85cc6a1afb91d80bbec07796ba3
r47orr <razor@arch.localdomain>
2022-06-22 00:48:24 -0300

    add base files and first client execution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("teste")
+const discord = require('discord.js')
+const config = require('./config.json')
+require('dotenv').config()
+
+client.once('ready', () => {
+    console.log('Bot on-line!')
+    console.log(`ID do bot: ${config.clientID}`)
+    console.log(`ID do servidor: ${config.guildId}`)
+})
