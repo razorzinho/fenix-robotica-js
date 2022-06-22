e4a6f8f6dc04da6b45aa708fba1368db9766bd31
r47orr <razor@arch.localdomain>
2022-06-22 01:50:50 -0300

    fix clientId syntax on index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
     console.log(`Bot on-line! Nome ${client.user.name}`)
-    console.log(`ID do bot: ${config.clientID}`)
+    console.log(`ID do bot: ${config.clientId}`)
     console.log(`ID do servidor: ${config.guildId}`)
     client.user.setPresence({ activities: [{ name: 'Observando fenixempire.net.br' }], status: 'online', type: 'WATCHING'} );
 })
