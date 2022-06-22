bd2c766de61cbbb2a38c768c4cd654a19473a264
r47orr <razor@arch.localdomain>
2022-06-22 01:40:08 -0300

    fix index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const discord = require('discord.js')
 const config = require('./config.json')
 require('dotenv').config()
 
+client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
 client.once('ready', () => {
-    console.log('Bot on-line!')
+    console.log(`Bot on-line! Nome ${client.user.name}`)
     console.log(`ID do bot: ${config.clientID}`)
     console.log(`ID do servidor: ${config.guildId}`)
+    client.user.setPresence({ activities: [{ name: 'Observando fenixempire.net.br' }], status: 'online', type: 'WATCHING'} );
 })
+
+client.login(process.env.TOKEN)
