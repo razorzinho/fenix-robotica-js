09c9ef826123b366be38f1c6a8a055da92cb8fa0
r47orr <razor@arch.localdomain>
2022-06-23 13:30:14 -0300

    try to fix index event reader

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const { Client, Intents } = require('discord.js');
 
 require('dotenv').config()
 
-client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client = new Client({ 
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.MESSAGE_CONTENT, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING],
+});
 
-const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, './events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
