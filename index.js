02d38de18921563ef3c92ec323a4102be5384381
r47orr <razor@arch.localdomain>
2022-06-23 13:40:41 -0300

    fix v2

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.MESSAGE_CONTENT, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING],
 });
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+//const eventsPath = path.join(__dirname, 'events');
+//const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.once('ready', client => {
+client.once('ready', bot => {
 	console.log('Iniciado!')
-	console.log(`Bot on-line! Nome ${client.user.username}`)
+	console.log(`Bot on-line! Nome ${bot.user.username}`)
 	console.log(`ID do bot: ${config.clientId}`)
 	console.log(`ID do servidor: ${config.guildId}`)
-	client.user.setActivity('fenixempire.net.br', { type: 'WATCHING' });
-	client.user.setStatus('dnd');
+	bot.user.setActivity('fenixempire.net.br', { type: 'WATCHING' });
+	bot.user.setStatus('dnd');
 });
 
 // for (const file of eventFiles) {
