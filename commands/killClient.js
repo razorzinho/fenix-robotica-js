1a0a0e249cf0becb9d91e536367f86eab9463e61
r47orr <razoroller529@gmail.com>
2023-03-22 02:18:43 -0300

    criar comando killClient
    * comando que força a desconexão do bot da API do Discord de forma segura

---
+++ commands/killClient.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kill')
+        .setDescription("Força o desligamento do bot desfazendo sua conexão com a API do Discord de forma segura."),
+    premissions: [
+        "1041417371417596006"
+    ],
+
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
+            .setColor('Red')
+            .setDescription('Finalizando conexão com a API do Discord...')
+            .setFooter({ text: `Tempo de duração desta sessão ${(interaction.client.uptime / 60000).toFixed(2)} minutos.`, iconURL: interaction.client.user.avatarURL()})
+            .setTimestamp()
+
+        await interaction.reply({ embeds: [embed] })
+
+        interaction.client.user.setPresence({ status: 'offline' })
+
+        interaction.client.destroy()
+    }
+}
