8986c41b3fa9aab7d201845412379fe68232c838
r47orr <razoroller529@gmail.com>
2023-03-22 02:17:09 -0300

    atualizar README.md e limpar index.js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # fenix-robotica - Rewrite em NodeJS
 
-// todo
+Esta aplicação consiste de um bot multitarefas, com diversas especialidades, construído utilizando os recursos mais modernos e úteis disponibilizados pelo Discord. 
+
+Seu funcionamento se baseia em eventos e comandos. Os eventos vão disparar funcionalidades específicas que estão ligadas a eles.
+
+Os principais recursos são o carregamento e envio de comandos automatizado (somente comandos (/)), bem como o sistema de permissões baseado em cargos do Discord, de configuração simples.
--- index.js
+++ index.js
@@ -1,32 +1,9 @@
-const {Client, Collection, GatewayIntentBits} = require("discord.js")
+const {Client, Collection} = require("discord.js")
 require("dotenv").config()
 const fs = require("node:fs")
 const path = require("node:path")
 
-// ------------------------------------------------------------------
-// Now, we must initialize the events for our client
-
-// não especificar intents para habilitar todos os permitidos ao bot
-const client = new Client({ intents: [3276799] }) // todos os intents por bits
-    // GatewayIntentBits.Guilds, 
-    // GatewayIntentBits.MessageContent, 
-    // GatewayIntentBits.DirectMessageReactions,
-    // GatewayIntentBits.DirectMessageTyping,
-    // GatewayIntentBits.DirectMessages,
-    // GatewayIntentBits.GuildModeration,
-    // GatewayIntentBits.GuildEmojisAndStickers,
-    // GatewayIntentBits.GuildIntegrations,
-    // GatewayIntentBits.GuildInvites,
-    // GatewayIntentBits.GuildMembers,
-    // GatewayIntentBits.GuildMessageReactions,
-    // GatewayIntentBits.GuildMessageTyping,
-    // GatewayIntentBits.GuildMessages,
-    // GatewayIntentBits.GuildPresences,
-    // GatewayIntentBits.GuildScheduledEvents,
-    // GatewayIntentBits.GuildVoiceStates,
-    // GatewayIntentBits.GuildWebhooks 
-    // --- todos os intents por GatewayIntentBits
-    // Sim, já sei que usar TUDO não é boa ideia, mas estou desenvolvendo, não importa.
+const client = new Client({ intents: [3276799] })
 
 client.commands = new Collection()
 
