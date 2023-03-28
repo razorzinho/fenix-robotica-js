313ce50bbd530f9a996cd3bdfc0f98e05cbfdea6
r47orr <razoroller529@gmail.com>
2023-03-28 02:22:49 -0300

    atualizar ClientReady.js
    * remover listagem de servidores detectados e sua funcionalidade desnecessária
    
    * utilizar for comum em vez de um forEach de coleção para iterar por servidores e sair dos que não estão permitidos
    
    * iniciar desenvolvimento de mensagens automáticas (com seus devidos componentes e embeds)

--- events/ClientReady.js
+++ events/ClientReady.js
@@ -1,7 +1,35 @@
-const { Events, ActivityType } = require("discord.js")
+const { Events, ActivityType, EmbedBuilder } = require("discord.js")
 const config = require('../config.json')
 const fs = require("node:fs")
 const path = require("node:path")
+
+function fetchGuildNames(client, guilds) {
+
+    for (item of guilds.values()) {
+
+        if (!config.client.allowed_guild_ids[item.id]) {
+
+            client.guilds.fetch(item.id).then( (guild) => {
+
+                guild.channels.fetch().then((col) => {
+                    let channel = col.find(x => x.isTextBased())
+
+                    channel.send("Eu não deveria estar aqui. Adeus. Sayonara. Adios. Goodbye.")
+                })
+
+                console.log(`Estou saindo de um servidor em que não deveria estar: ${guild.name} (${guild.id})\nNão está na lista de servidores permitidos.`)
+
+                guild.leave()
+
+            } )
+
+            continue
+
+        }
+
+    }
+
+}
 
 module.exports = {
 
@@ -9,38 +37,17 @@ module.exports = {
     once: true,
     
     execute(client) {
-        let guilds = ''
 
-        const col = client.guilds.cache
+        console.log("Verificando servidores atuais...")
 
-        const colSize = client.guilds.cache.size
+        client.guilds.fetch().then((guilds) => {
 
-        let counter = 1
-
-        col.forEach((guild) => {
-
-            if (!config.client.allowed_guild_ids[guild.id]) {
-                const channel = guild.channels.cache.toJSON().find(x => x.isTextBased())
-                channel.send("Eu não deveria estar aqui. Adeus. Sayonara. Adios. Goodbye.")
-
-                console.log(`Estou saindo de um servidor em que não deveria estar: ${guild.name} (${guild.id})`)
-
-                guild.leave()
-            }
-
-            if (counter == colSize) {
-                guilds += guild.name
-
-                return
-            }
-
-            guilds += guild.name + ", "
-
-            counter++
+            fetchGuildNames(client, guilds)
+            
         })
 
-        const commandsPath = path.join(__dirname, "../commands")
-        const commandFiles = fs.readdirSync(commandsPath).filter(x => x.endsWith(".js"))
+        const commandsPath = path.join(__dirname, '../commands')
+        const commandFiles = fs.readdirSync(commandsPath).filter(x => x.endsWith('.js'))
 
         let commandsData = []
         for (const cFile of commandFiles) {
@@ -49,7 +56,7 @@ module.exports = {
             const command = require(commandPath)
 
             if (!command.data || !command.execute) {
-                console.log(`${cFile.replace(".js", "")} não é um comando válido. Continuando.`)
+                console.log(`${cFile.replace('.js', '')} não é um comando válido. Ignorando.`)
 
                 continue
             }
@@ -59,15 +66,24 @@ module.exports = {
             commandsData.push(command.data)
         }
 
-        client.application.commands.set(commandsData, config.client.dev_guild_id)
+        client.application.commands.set(commandsData, config.client.dev_guild_id).then((commands) => console.log(`Registrei com êxito ${commands.size} comandos (/).`))
 
         client.user.setPresence({activities: [{name: "Sendo desenvolvido", type: ActivityType.Watching}], status: 'dnd'})
 
         // faremos disso um sistema automatizado de envio de mensagens predefinidas de acordo com módulos que as usam
-        // for (guildId in config.client.allowed_guild_ids) {
-        //     let guild = client.guilds.fetch(guildId)
-        // }
+        for (guildId in config.client.allowed_guild_ids) {
+            let guild = client.guilds.fetch(guildId)
+
+            // console.log(`No servidor ${guild.name}:\n`)
         
-        console.log(`Conexão estabelecida! Conectado como ${client.user.tag}. \nServidores detectados: ${guilds}`)
+            for (main in config.modules.automatic) {
+                console.log(main)
+            }
+
+            // inicializar sistema de tickets de alguma forma...
+        }
+
+            console.log(`Conexão estabelecida! Conectado como ${client.user.tag}.`)
+        }
+
     }
-}
