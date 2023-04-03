5b722f541bdcb877b9f748021fc3705cdc37eda6
r47orr <razoroller529@gmail.com>
2023-04-03 04:25:41 -0300

    remover temporariamente carregamento de componentes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("node:path")
 const client = new Client({ intents: [3276799] })
 
 client.commands = new Collection()
-client.components = new Collection()
+// client.components = new Collection()
 
 const eventsPath = path.join(__dirname, "events")
 const eventFiles = fs.readdirSync(eventsPath).filter(x => x.endsWith('.js'))
@@ -28,23 +28,23 @@ for (const eFile of eventFiles) {
     client.once(event.name, (...args) => event.execute(...args))
 }
 
-const componentsPath = path.join(__dirname, "components")
-const componentFiles = fs.readdirSync(componentsPath).filter(x => x.endsWith('.js'))
+// const componentsPath = path.join(__dirname, "components")
+// const componentFiles = fs.readdirSync(componentsPath).filter(x => x.endsWith('.js'))
 
-console.log('Inicializando componentes...')
+// console.log('Inicializando componentes...')
 
-for (const cFile of componentFiles) {
-    const componentPath = path.join(eventsPath, cFile)
+// for (const cFile of componentFiles) {
+//     const componentPath = path.join(eventsPath, cFile)
 
-    const component = require(componentPath)
+//     const component = require(componentPath)
 
-    if (!component.data || !component.execute) {
-        console.log(`${cFile.replace('.js', '')} não é um componente válido. Ignorando.`)
+//     if (!component.data || !component.execute) {
+//         console.log(`${cFile.replace('.js', '')} não é um componente válido. Ignorando.`)
 
-        continue
-    }
+//         continue
+//     }
 
-    client.components.set(components.data.customId, components)
-}
+//     client.components.set(components.data.customId, components)
+// }
 
 client.login(process.env.TOKEN)
