a2ff1ac636586dc99bf386577e8ccfb88fb72e82
r47orr <razoroller529@gmail.com>
2023-04-03 04:25:25 -0300

    corrigir traduções do comando tópico

--- commands/topic.js
+++ commands/topic.js
@@ -1,39 +1,48 @@
-const {SlashCommandBuilder} = require("discord.js")
+const {SlashCommandBuilder, EmbedBuilder, channelMention, codeBlock} = require("discord.js")
 
 const localisedDenialReplies = {
-    'en-US': 'This channel has no defined topic! Try another one, or ask the server owner to define one!',
     'en-GB': 'This channel has no defined topic! Try another one, or ask the server owner to define one!',
+    'en-US': 'This channel has no defined topic! Try another one, or ask the server owner to define one!',
     'pt-BR': 'Este canal não tem um tópico definido! Tente em outro, ou peça ao dono do servidor para definir um!'
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('topic')
-        .setNameLocalizations({
-            'en-US': 'topic',
+        .setNameLocalizations( {
             'en-GB': 'topic',
-            'pt-BR': 'topico'
-        })
-        .setDescriptionLocalizations({
-            'en-US': 'Sends the current channel\'s topic description, if there is one.',
+            'en-US': 'topic',
+            'pt-BR': 'tópico'
+        } )
+        .setDescription('Sends the current channel\'s topic description, if there is one.')
+        .setDescriptionLocalizations( {
             'en-GB': 'Sends the current channel\'s topic description, if there is one.',
+            'en-US': 'Sends the current channel\'s topic description, if there is one.',
             'pt-BR': 'Envia a descrição de tópico do canal em que foi requisitado, se houver uma.'
-        }),
-
+        } ),
+    
     async execute(interaction) {
-        if (!interaction.channel.topic) {
-            await interaction.reply(`${localisedDenialReplies[interaction.locale]}`)
 
-            return
-        }
+        const topic = interaction.channel.topic != undefined ? interaction.channel.topic : ''
 
         const reply = {
-            'en-US': `This channel's topic ${codeBlock({topic})}.`,
-            'en-GB': `This channel's topic ${codeBlock({topic})}.`,
-            'pt-BR': `O tópico deste canal é ${codeBlock({topic})}.`
+            'en-GB': `This channel's topic is: \`${topic}\`.`,
+            'en-US': `This channel's topic is: \`${topic}\`.`,
+            'pt-BR': `O tópico deste canal é: \`${topic}\`.`
         }
 
-        await interaction.reply(reply[interaction.locale])
-    }
+        let content = interaction.channel.topic != undefined ? reply[interaction.locale] : localisedDenialReplies[interaction.locale]
 
+        const embed = new EmbedBuilder()
+            .setColor( interaction.channel.topic != undefined ? 'DarkGreen' : 'DarkRed' )
+            .setAuthor({name: interaction.user.tag, iconURL: interaction.user.avatarURL()})
+            .addFields({ name: channelMention(interaction.channel.id), value: content })
+            .setTimestamp()
+
+        await interaction.reply({ embeds: [embed]}).then(
+            (message) => {
+                setTimeout(async () => await message.delete(), 15000)
+            }
+        )
+    }
 }
